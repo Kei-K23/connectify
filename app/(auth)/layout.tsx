4c5e8f556983d21c24da5ef447960d9780ab5df0
Kei-K23 <keiksl2301@gmail.com>
2024-01-17 21:52:06 +0630

    setup light/dark mode

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <main className="h-full w-full flex flex-col items-center justify-center">
-      <div className="fixed top-0 text-center bg-black text-white w-full py-4">
+      <div className="fixed top-0 text-center bg-black dark:bg-neutral-100 dark:text-black text-white w-full py-4">
         <h1 className="text-2xl md:text-3xl font-semibold mb-2">Connectify</h1>
         <h2 className="text-lg md:text-xl font-bold">
           Share what you see, hear, and think to the world

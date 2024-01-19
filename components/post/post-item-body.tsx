0164255c9d388b6a2fcfb901cab4e61e585e8f7c
Kei-K23 <keiksl2301@gmail.com>
2024-01-20 01:28:45 +0630

    add post item action icons

--- components/post/post-item-body.tsx
+++ components/post/post-item-body.tsx
@@ -1,8 +1,10 @@
+"use client";
 import { PostWithProfile } from "@/type";
 import Image from "next/image";
 import React from "react";
 import VerticalSeparator from "../vertical-separator";
 import { HeartIcon, MessageCircle } from "lucide-react";
+import { Button } from "../ui/button";
 
 interface PostItemBodyProps {
   data: PostWithProfile;
@@ -35,7 +37,16 @@ const PostItemBody = ({ data }: PostItemBodyProps) => {
           />
         </div>
       )}
-      <div></div>
+      <div className="flex items-center gap-x-2 mt-3">
+        {postActions.map((a) => {
+          const Icon = a.icon;
+          return (
+            <Button key={a.label} variant={"ghost"} size={"xs"}>
+              <Icon className="w-6 h-6  stroke-rose-500 fill-rose-500" />
+            </Button>
+          );
+        })}
+      </div>
     </div>
   );
 };

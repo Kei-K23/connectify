5d5053f9f6052ffa7cd91575f0f4922af386dd2b
Kei-K23 <keiksl2301@gmail.com>
2024-01-25 12:49:27 +0630

    add profile body for profile page

--- app/(main)/(profile)/[username]/page.tsx
+++ app/(main)/(profile)/[username]/page.tsx
@@ -2,6 +2,9 @@ import { getCurrentUser } from "@/services/user-service";
 import { notFound } from "next/navigation";
 import React from "react";
 import ProfileHeader from "./_components/profile-header";
+import { getPostByProfileId } from "@/services/post-service";
+import PostItem from "@/components/post/post-item";
+import { PostWithAll } from "@/type";
 
 interface ProfilePageProps {
   params: {
@@ -18,9 +21,28 @@ const ProfilePage = async ({ params }: ProfilePageProps) => {
     return notFound();
   }
 
+  const posts = await getPostByProfileId(profile?.id);
+
   return (
     <div>
       <ProfileHeader profile={profile} otherProfile={otherProfile} />
+
+      {posts.length ? (
+        <>
+          <h2 className="text-center my-6 text-muted-foreground">Posts</h2>
+          <div className="space-y-8 mt-4 pb-14">
+            {posts.map((post) => (
+              <PostItem<PostWithAll>
+                key={post.id}
+                data={post}
+                profile={profile!}
+              />
+            ))}
+          </div>
+        </>
+      ) : (
+        <h2 className="text-center text-muted-foreground mt-4">No posts.</h2>
+      )}
     </div>
   );
 };
--- services/post-service.ts
+++ services/post-service.ts
@@ -116,6 +116,63 @@ export async function getPostById(id: string) {
   }
 }
 
+export async function getPostByProfileId(id: string) {
+  try {
+    const profile = await getCurrentUser();
+
+    return await db.post.findMany({
+      where: {
+        profileId: id,
+        AND: [
+          {
+            mutes: {
+              every: {
+                NOT: {
+                  profileId: profile?.id,
+                },
+              },
+            },
+          },
+          {
+            profile: {
+              NOT: {
+                OR: [
+                  {
+                    blockings: {
+                      some: {
+                        blockerId: profile?.id,
+                      },
+                    },
+                  },
+                  {
+                    blockers: {
+                      some: {
+                        blockingId: profile?.id,
+                      },
+                    },
+                  },
+                ],
+              },
+            },
+          },
+        ],
+      },
+      include: {
+        profile: {
+          include: {
+            followers: true,
+            followings: true,
+          },
+        },
+        replies: true,
+        likes: true,
+      },
+    });
+  } catch (e: any) {
+    throw new Error("Internal server error!");
+  }
+}
+
 //! Have to retrieve correctly
 export async function getPostByNameAndProfileName(term: string) {
   try {
